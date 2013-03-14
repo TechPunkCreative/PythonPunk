d72dbc052859a46e815330fd990ab41e83a98efb
Nathan Stevens <8nathanstevens8@gmail.com>
2013-03-14 12:34:24 -0400

    Version 0.1 of PRE-Beta
    I recomend waiting till BETA(not PRE-Beta) As there will be alot of
    story missing

---
+++ PythonPunk0.1.py
@@ -0,0 +1,60 @@
+#-------------------------------------------------------------------------------
+# Name:        PythonPunk0.1
+# Purpose:     For all to enjoy
+#
+# Author:      Nathan
+#
+# Created:     14/03/2013
+# Copyright:   (c) Nathan 2013
+# Licence:     Attribution-NonCommercial-NoDerivs 3.0 Unported
+#-------------------------------------------------------------------------------
+import sys
+import time
+from sys import argv
+print("Welcome to the hi-tech world of PythonPunk!")
+time.sleep(3)
+print("In this world there is a war")
+time.sleep(3)
+print("It is between the United Planets rulled by the emporor and a rouge general and his army")
+time.sleep(4)
+print("--------------")
+print(" CHAPTER 0 ")
+print("--------------")
+print("You have been recruited into the world through a time-travel portal")
+time.sleep(3)
+print("You find yourself in what look likse a small starship wth two men in armor")
+time.sleep(3)
+plrname = input("Tell us your name. Asks the man that brought you throught the portal")
+time.sleep(3)
+print("Nice to meet you " + str(plrname) + ". Said the other man.")
+time.sleep(3)
+print("Right as he said that he stood up as the computer said 'WARNING incoming blaster fire detected WARNING!'")
+time.sleep(5)
+# At this time you would be able to chose a class but only one till we get to version 2, so no need to ask
+print("'Here take this'.  Said the man as he threw you a assualt rifle.")
+time.sleep(3)
+print("Were going down! said the man that got up earlier")
+time.sleep(3)
+print("3 hours later after you crashed")
+time.sleep(3)
+print("Hey this ones awake!")
+time.sleep(3)
+print("Where am I? you asked")
+time.sleep(3)
+print("Your ship went down and your two guards didnt make it, may I ask for your name?")
+time.sleep(3)
+print("The names " + str(plrname) + ". You said to the Medical Officer")
+time.sleep(3)
+print("The room was sudenly silent and a man who had a Commanders pin walked in")
+time.sleep(3)
+print("Whats going on? you asked")
+time.sleep(3)
+print("Welcome to templar squad, soldier. said the commander")
+time.sleep(3)
+print("Its about time somone filled you in, you are a soldier who is going to lead the rebelion after this war is over but we needed you now.")
+time.sleep(4)
+print("--------------")
+print(" CHAPTER 1 ")
+print(" Templar Squad ")
+print("--------------")
+
